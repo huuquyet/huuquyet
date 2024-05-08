9e436898920617ecb68149d510d38249dcb109bc
huuquyet <huuquyetng@gmail.com>
2024-05-09 02:18:12 +0700

    feat: install jq if not exist

--- quote_of_the_day.sh
+++ quote_of_the_day.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Check if jq exist to get json response
+if ! command -v jq &> /dev/null; then
+  echo "Install jq to get json response"
+  sudo apt install jq -y
+fi
+
+# Comment block in READEME.md to insert the quotes
 START="<!-- START_QUOTE -->"
 END="<!-- END_QUOTE -->"
-
 # sed -n '/'"$START"'/,/'"$END"'/{//!p}' README.md
 # sed -n '/START_QUOTE/,/END_QUOTE/{//!p}' README.md
 
@@ -16,15 +22,15 @@ DISPLAY=' \
   > *- '"$AUTHOR"' -*\n'
 
 # sed multiline replacement between 2 patterns https://fahdshariff.blogspot.com/2012/12/sed-mutli-line-replacement-between-two.html
-sed -n -e '/START_QUOTE/{              # when "START_QUOTE" is found
-            p                       # print
+sed -n -e '/START_QUOTE/{           # when "START_QUOTE" is found
+            p                       # print the first comment block
             :a                      # create a label "a"
             N                       # store the next line
             /END_QUOTE/!ba          # goto "a" and keep looping and storing lines until "END_QUOTE" is found
            # N                      # store the line containing "END_QUOTE"
             s/.*\n/'"$DISPLAY"'\n/  # replace the content
         }
-        p' -i README.md                # print
+        p' -i README.md             # print
 
 # one line version
-# sed -n '/START_QUOTE/{p;:a;N;/END_QUOTE/!ba;s/.*\n/'"$DISPLAY"'\n/};p' README.md
+# sed -n -e '/START_QUOTE/{p;:a;N;/END_QUOTE/!ba;s/.*\n/'"$DISPLAY"'\n/};p' -i README.md
